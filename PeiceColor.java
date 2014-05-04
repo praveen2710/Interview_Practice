e4ff314180eac0e6153cb56c2bb1762aec9c5f56
Praveen Banthia <praveenbanthia@Praveens-MacBook-Pro.local>
2014-05-04 19:19:13 -0400

    v1.3

---
+++ PeiceColor.java
@@ -0,0 +1,3 @@
+public enum PeiceColor{
+  Black,White;
+}
---
+++ enumExample.java
@@ -0,0 +1,15 @@
+enum Peice{
+  PAWN,KING,QUEEN,ROOK,BISHOP,KNIGHT
+}
+
+public class enumExample{
+  
+  public static void main(String args){
+    Peice p;
+    PeiceColor pc;
+    validMoves(int[][] Sqaure);
+    int[][] placeAt;
+    boolean moved; 
+  }
+
+}
